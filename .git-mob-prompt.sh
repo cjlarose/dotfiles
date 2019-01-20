7bf3a1587ed29defdf410707efee3f52d9e010af
Chris LaRose <cjlarose@gmail.com>
2019-01-20 11:50:17 -0800

    Go back to looking up co-authors one-by-one
    This way there's some feedback if we couldn't find a co-author by email

--- .git-mob-prompt.sh
+++ .git-mob-prompt.sh
@@ -1,20 +1,23 @@
 function __get_coauthors_by_email()
 {
   local emails=("$@")
-  local literals=()
+  local names=()
   for email in "${emails[@]}"; do
-    literals+=('"'"$email"'"')
+    local filter='.coauthors | to_entries[] | select(.value.email == "'"$email"'") | .key'
+    if name=$(jq --exit-status --raw-output "$filter" ~/.git-coauthors); then
+      names+=("$name")
+    else
+      names+=('[unknown]')
+    fi
   done
-  local search='['${(j:,:)literals}']'
-  local filter='.coauthors | to_entries[] | select([.value.email] | inside('"$search"')) | .key'
-  jq --exit-status --raw-output "$filter" ~/.git-coauthors
+  echo "${names[@]}"
 }
 
 function __git_mob_ps1()
 {
   if current_mob=$(git config --get-all git-mob.co-author); then
     local emails=("${(@f)$(gsed -n 's/.*<\(.*\)>/\1/p' <<< "$current_mob")}")
-    local names=("${(@f)$(__get_coauthors_by_email "${emails[*]}")}")
+    local names=("${(@f)$(__get_coauthors_by_email "${emails[@]}")}")
     echo "${names[@]}"
   fi
 }
