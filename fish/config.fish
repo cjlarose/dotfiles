d0b4a5d0d65330f30507e25564295073b747074a
Chris LaRose <cjlarose@gmail.com>
2019-09-23 15:28:20 -0700

    Add aliases to fish

--- fish/config.fish
+++ fish/config.fish
@@ -3,5 +3,13 @@ set -x EDITOR nvim
 set -x LESS '--quit-if-one-screen --RAW-CONTROL-CHARS --no-init'
 set -x THOR_MERGE 'nvr -s -d'
 
+# aliases
+alias ll='ls -al'
+alias be='bundle exec'
+alias rtest='env SEED=1337 bundle exec ruby -Itest'
+alias ptest='env MYCASE__TESTING__PARALLELISM__ENABLED=1 SEED=1337 bundle exec ruby -Itest'
+alias crtest='env CHROME_DEBUG=1 SEED=1337 bundle exec ruby -Itest'
+alias vim='nvim'
+
 # asdf
 source ~/.asdf/asdf.fish
