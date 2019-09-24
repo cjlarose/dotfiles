6c0779fc1c6df04a08b3e91d54c794aada319a65
Chris LaRose <cjlarose@gmail.com>
2019-09-23 18:19:16 -0700

    Separate out function prompt_git_branch

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,11 +1,16 @@
-function fish_right_prompt
-  set -l git_branch_color 'yellow'
+function prompt_git_branch
   set -l repo_info (git rev-parse --git-dir --is-inside-git-dir \
     --is-bare-repository --is-inside-work-tree \
     --short HEAD 2>/dev/null)
   and begin
     set -l branch (git branch | sed -n '/\* /s///p' 2>/dev/null)
-    set_color $git_branch_color
     echo "$branch"
   end
+end
+
+function fish_right_prompt
+  set -l git_branch_color 'yellow'
+  set_color $git_branch_color
+  prompt_git_branch
+  set_color normal
 end
