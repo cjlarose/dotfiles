f1f89e0c3a926f1510f4502b22d21d4f147bf5a7
Chris LaRose <cjlarose@gmail.com>
2019-09-24 08:43:49 -0700

    Remove checkout shortcut in favor of git branch widget

--- fish/functions/fbr.fish
+++
@@ -1,7 +0,0 @@
-# fbr - checkout git branch (including remote branches), sorted by most recent commit, limit 30 last branches
-# Adapted for fish from https://github.com/junegunn/fzf/wiki/examples#git
-function fbr
-  set -l branches (git branch --all | grep -v HEAD)
-  and set -l branch (string join \n $branches | fzf --no-multi --height 40% --reverse)
-  and git checkout (echo $branch | sed 's/.* //' | sed 's#remotes/[^/]*/##')
-end
