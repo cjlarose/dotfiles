c1caa5f1b59760ef8e1df523519aedc278d192d7
Chris LaRose <cjlarose@gmail.com>
2019-09-23 18:33:29 -0700

    Add git mob to prompt

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -8,9 +8,21 @@ function prompt_git_branch
   end
 end
 
+function prompt_git_mob
+  set -l initials (git mob-print --initials)
+  if test -n "$initials"
+    echo -n -s '(with ' $initials ')'
+  end
+end
+
 function fish_right_prompt
   set -l git_branch_color 'yellow'
   set_color $git_branch_color
   prompt_git_branch
   set_color normal
+
+  set -l mob (prompt_git_mob)
+  if test -n "$mob"
+    echo -n " $mob"
+  end
 end
